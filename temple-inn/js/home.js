fa3dd94f3e435544f308393ee54bd455a3850a86
guibyiu <guimn@byui.edu>
2019-12-10 19:08:57 -0700

    toggle menu working

---
+++ temple-inn/js/home.js
@@ -0,0 +1,10 @@
+function toggleMenuButton() {
+   var x = document.getElementById("myLinks");
+   if (x.style.display === "block") {
+     x.style.display = "none";
+   } else {
+     x.style.display = "block";
+   }
+ }
+
+ toggleMenuButton();
