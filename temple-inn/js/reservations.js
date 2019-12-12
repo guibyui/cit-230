d98b2f18a6b35e61ee687f168c584b4c05e5ba45
guibyiu <guimn@byui.edu>
2019-12-12 15:16:05 -0700

    Session storage data displaying on confirmation page

--- temple-inn/js/reservations.js
+++ temple-inn/js/reservations.js
@@ -4,9 +4,42 @@ var sessStore = window.sessionStorage;
 window.addEventListener('DOMContentLoad', getData());
 
 function getData() {
-   let firstName = document.getElementById("fname");
-   firstName.innerHTML = sessStore.getItem("firstName");
+   let firstNameId = document.getElementById("firstNameId");
+   firstNameId.innerHTML = sessStore.getItem("firstName");
+
+   let lastNameId = document.getElementById("lastNameId");
+   lastNameId.innerHTML = sessStore.getItem("lastName");
+
+   let emailAddressId = document.getElementById("emailAddressId");
+   emailAddressId.innerHTML = sessStore.getItem("email");
    
+   let phoneNumberId = document.getElementById("phoneNumberId");
+   phoneNumberId.innerHTML = sessStore.getItem("phone");
+
+   let countryId = document.getElementById("countryId");
+   countryId.innerHTML = sessStore.getItem("country");
+
+   let LocationId = document.getElementById("LocationId");
+   LocationId.innerHTML = sessStore.getItem("location");
+
+   let checkInDateId = document.getElementById("checkInDateId");
+   checkInDateId.innerHTML = sessStore.getItem("check-in-date");
+
+   let checkOutDateId = document.getElementById("checkOutDateId");
+   checkOutDateId.innerHTML = sessStore.getItem("check-out-dateId");
+
+   let numberOfGuestsId = document.getElementById("numberOfGuestsId");
+   numberOfGuestsId.innerHTML = sessStore.getItem("numberOfGuests");
+
+   let yesMealsId = document.getElementById("yesMealsId");
+   yesMealsId.innerHTML = sessStore.getItem("yesMeals");
+
+   let noMealsId = document.getElementById("noMealsId");
+   noMealsId.innerHTML = sessStore.getItem("noMeals");
+
+   let balconyId = document.getElementById("balconyId");
+   balconyId.innerHTML = sessStore.getItem("balcony");
+
 }
 
 
