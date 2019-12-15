70a7e76f6b2740b1bed7b298736f776d4de3b21e
guibyiu <guimn@byui.edu>
2019-12-14 17:44:38 -0700

    toggle fixed

--- temple-inn/js/home.js
+++ temple-inn/js/home.js
@@ -3,12 +3,12 @@
 var sessStore = window.sessionStorage;
 
 function onPageLoad() {
-   //update copyright date
-   var todaysDate = new Date();
-   var currentYear = todaysDate.getFullYear();
-   document.getElementById('copyright-year').innerHTML = currentYear;
-   //get document.lastmodified
-   document.getElementById('lastupdated').innerHTML = document.lastModified;
+  //update copyright date
+  var todaysDate = new Date();
+  var currentYear = todaysDate.getFullYear();
+  document.getElementById('copyright-year').innerHTML = currentYear;
+  //get document.lastmodified
+  document.getElementById('lastupdated').innerHTML = document.lastModified;
 }
 //calls funciton after page has loaded
 window.onload = onPageLoad();
@@ -25,54 +25,53 @@ window.onload = onPageLoad();
 
 // toggleMenuButton();
 
-
 function toggleMenuButton() {
-   var x = document.getElementById("myTopnav");
-   if (x.className === "topnav") {
-     x.className += " responsive";
-   } else {
-     x.className = "topnav";
-   }
- }
- 
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
 var submit = document.getElementById("submition");
 
 submit.addEventListener("click", function () {
-   console.log("It Works!!");
-   sessStore.firstName = document.getElementById("first-name").value;
+  console.log("It Works!!");
+  sessStore.firstName = document.getElementById("first-name").value;
 
-   let lastName = document.getElementById("last-name").value;
-   sessStore.setItem("last-name", lastName);
+  let lastName = document.getElementById("last-name").value;
+  sessStore.setItem("last-name", lastName);
 
-   let email = document.getElementById("email").value;
-   sessStore.setItem("email", email);
+  let email = document.getElementById("email").value;
+  sessStore.setItem("email", email);
 
-   let phone = document.getElementById("phone").value;
-   sessStore.setItem("phone", phone);
+  let phone = document.getElementById("phone").value;
+  sessStore.setItem("phone", phone);
 
-   let country = document.getElementById("country").value;
-   sessStore.setItem("country", country);
+  let country = document.getElementById("country").value;
+  sessStore.setItem("country", country);
 
-   let location = document.getElementById("location").value;
-   sessStore.setItem("location", location);
+  let location = document.getElementById("location").value;
+  sessStore.setItem("location", location);
 
-   let checkIn = document.getElementById("check-in-date").value;
-   sessStore.setItem("check-in-date", checkIn);
+  let checkIn = document.getElementById("check-in-date").value;
+  sessStore.setItem("check-in-date", checkIn);
 
-   let checkOut = document.getElementById("check-out-date").value;
-   sessStore.setItem("check-out-date", checkOut);
+  let checkOut = document.getElementById("check-out-date").value;
+  sessStore.setItem("check-out-date", checkOut);
 
-   let guests = document.getElementById("numberOfGuests").value;
-   sessStore.setItem("numberOfGuests", guests);
+  let guests = document.getElementById("numberOfGuests").value;
+  sessStore.setItem("numberOfGuests", guests);
 
-   let yesMeals = document.getElementById("yesMeals").checked;
-   sessStore.setItem("yesMeals", yesMeals);
+  let yesMeals = document.getElementById("yesMeals").checked;
+  sessStore.setItem("yesMeals", yesMeals);
 
-   let noMeals = document.getElementById("noMeals").checked;
-   sessStore.setItem("noMeals", noMeals);
+  let noMeals = document.getElementById("noMeals").checked;
+  sessStore.setItem("noMeals", noMeals);
 
-   let balcony = document.getElementById("balcony").checked;
-   sessStore.setItem("balcony", balcony);
+  let balcony = document.getElementById("balcony").checked;
+  sessStore.setItem("balcony", balcony);
 });
 
 
